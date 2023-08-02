7bb9a7b195114b162420569c6dbb745a0066a1a6
VishalSingh2610 <85185564+VishalSingh2610@users.noreply.github.com>
2023-08-02 07:53:25 +0530

    This is Assignment of Lets upgrade 3 days

---
+++ EvenOrOdd.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a Number: "<<endl;
+    cin>>n;
+
+    if(n%2==0){
+        cout<<"Number is even."<<endl;
+    }
+    else{
+        cout<<"Number is odd."<<endl;
+    }
+}
